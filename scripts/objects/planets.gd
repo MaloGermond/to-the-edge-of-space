c38085b5be2b160830ead7acef0402c0127ac752
Malo Germond <github@matchalo.org>
2026-02-04 22:14:08 +0100

    :sparkles: Allows to change sprite planet texture

--- scripts/objects/planets.gd
+++ scripts/objects/planets.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 @export var angular_speed = 0.001
+@export var sprite: Sprite2D
+@export var albedo_texture: Texture2D
+# @export var normal_texture: Texture2D
+	
+func _ready():
+	if albedo_texture:
+		print(albedo_texture)
+		sprite.texture.diffuse_texture = albedo_texture
 	
 func _process(delta: float) -> void:
 	rotation += angular_speed
